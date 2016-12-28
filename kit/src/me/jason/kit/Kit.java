fb0a95d56b08c48933130b44a587b7bd07d7fa18
Jason Szot <jszot1982@hotmail.com>
2016-12-28 03:14:27 -0600

    tutorial 9 - adding hooks into economy plugins

--- kit/src/me/jason/kit/Kit.java
+++ kit/src/me/jason/kit/Kit.java
@@ -7,12 +7,22 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.md_5.bungee.api.ChatColor;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
 
 public class Kit extends JavaPlugin{
+	public static Economy econ = null;
+	
 	public void onEnable(){
+		if (!setupEconomy() ) {
+            Bukkit.getServer().getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 		Bukkit.getServer().getLogger().info("kit v" + this.getDescription().getVersion() + " enabled.");
 	}
 	
@@ -29,13 +39,20 @@ public class Kit extends JavaPlugin{
 		Player p = (Player) sender;
 		
 		if (cmd.getName().equalsIgnoreCase("kit")){
-			// give the player some items
-			PlayerInventory pi = p.getInventory();
-			pi.addItem(new ItemStack(Material.STONE_SWORD, 1));
-			pi.addItem(new ItemStack(Material.STONE_PICKAXE, 1));
-			pi.addItem(new ItemStack(Material.APPLE, 5));
-			p.sendMessage(ChatColor.GREEN + "You got your kit!");
-			return true;
+			// give the player some items, cost 10 currency
+			@SuppressWarnings("deprecation")
+			EconomyResponse r = econ.withdrawPlayer(p.getName(), 10);
+			if (r.transactionSuccess()){
+				PlayerInventory pi = p.getInventory();
+				pi.addItem(new ItemStack(Material.STONE_SWORD, 1));
+				pi.addItem(new ItemStack(Material.STONE_PICKAXE, 1));
+				pi.addItem(new ItemStack(Material.APPLE, 5));
+				p.sendMessage(ChatColor.GREEN + "You got your kit!");
+				return true;
+			}else{
+				p.sendMessage(ChatColor.RED + "You cannot get a kit at this time.");
+				return true;
+			}
 		}
 		
 		if (cmd.getName().equalsIgnoreCase("ci")){
@@ -49,11 +66,27 @@ public class Kit extends JavaPlugin{
 		return true;
 	}
 
+	
+	// setup econ from vault api page
+	
+	private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
 }
 
 /*
  *	tutorial on YouTube 
- * 	youtube.com/watch?v=NVNSfUjXJow
+ * 	youtube.com/watch?v=NVNSfUjXJow - part 1 - basic inventory stuff
+ *  youtube.com/watch?v=GeIkAhdavMg - part 2 - hooking into another plugin for economy (using Vault)
  *  youtube.com/user/PogoStick29Dev
  * 	
  */
